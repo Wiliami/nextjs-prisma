5d5a6418a08016328cf3f5e1ef1ba6ae5938be25
wiliami <wiliaup97@gmail.com>
2026-01-06 16:32:20 -0400

    chore: add redirect users authenticated

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,5 +1,15 @@
-import { SiginForm } from '../../../components/signin-form'
+"use client"
+
+import { authClient } from '../../../lib/auth-client'
+import { SiginForm } from '../../../components/signin-form';
+import { redirect } from 'next/navigation'
 
 export default function SignInPage() {
+    const { data } = authClient.useSession();
+
+    if(data?.session) {
+        redirect('/dashboard')
+    }
+    
     return <SiginForm />
 }
