7bd570d78e04eeba36167359d0442791e31bdf8a
wiliami <wiliaup97@gmail.com>
2026-02-12 20:01:10 -0400

    chore: add configs and fix

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-<<<<<<< HEAD
-
-=======
->>>>>>> ec220928b1b7c142a80f684ee5bbafc5b3bfdbc3
   logging: {
     fetches: {
       fullUrl: true
