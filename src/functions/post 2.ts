1bd688dc782fbf47745314fe52dc8df6e43503aa
wiliami <wiliaup97@gmail.com>
2026-04-19 16:37:04 -0400

    feat: add functions

---
+++ src/functions/post 2.ts
@@ -0,0 +1,116 @@
+/* Injeção de dependência */
+
+interface INotificator {
+    enviar: () => void
+}
+
+class Notificator {
+    enviar() {
+        console.log('-> Notificando usuários')
+    }
+
+    handleMessageNotification() {
+        console.log('-> Manipulador para mensagens de notificação!')
+    }
+}
+
+
+class Post {   
+    constructor(public titulo: string) {
+        console.log('-> Novo Post: ' + titulo)
+    }
+
+    publicar() {
+        console.log('-> Publicando Post!');
+        let notificador = new Notificator();
+        notificador.enviar()
+    }
+}
+
+class Comentario {
+    criar() {
+        console.log('-> Criando comentário!')
+        let notificador = new Notificator()
+        notificador.enviar();
+    }
+}
+
+
+let novoPost = new Post('Aprendendo injeção de dependência!')
+novoPost.publicar()
+
+
+
+
+
+/*  Imperativa */  
+/* Faz um for, depois um if e adiciona no array aux e depois retorna o array /*
+/* 
+const getOlderStudents = (students) => {
+    let aux = [];
+    for(student of students) {
+        if(student >= 18) {
+            aux.push(student)
+        }
+    }
+
+    return aux;
+}
+*/
+
+
+/* Declarativa */
+/* Filtra os estudantes maiores de 18 anos */
+// const getOlderStudents = (students) => students.filter(student => student >= 18) 
+const getOlderStudent = (students: any[]) => {
+    students.filter(student => {
+        return student >= 18;
+    })
+
+    return students
+}
+
+
+
+function soma(a: number, b: string) {
+    return a + b;
+}
+
+
+class Mathlib {
+    sum(a: number, b: number) {
+        return a + b;
+    }
+}
+
+
+// const sum = new Mathlib()
+// console.log(sum.sum(1, 2));
+
+
+class Retangulo {
+    altura
+    largura
+    constructor(altura: number, largura: number) {
+        this.altura = altura
+        this.largura = largura
+    }
+}
+
+// const retangulo = new Retangulo(1, 1)
+// console.log(retangulo)
+
+
+
+class Utils {
+    static sum(a: number, b: number) {
+        return a + b;
+    }
+
+    static formatName(name: string) {
+        return name.trim().toUpperCase();
+    }
+}
+
+console.log(Utils.sum(1, 2));
+console.log(Utils.formatName('Wiliamis'))
