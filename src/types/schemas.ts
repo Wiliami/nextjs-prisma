eb3b6785e58ea3fdc308b402e5d7a95b32a6f0a9
wiliami <wiliaup97@gmail.com>
2026-04-21 16:23:01 -0400

    feat: add schema

--- src/types/schemas.ts
+++ src/types/schemas.ts
@@ -1,4 +1,12 @@
+import z from "zod"
+
 export interface Posts {
     id: number,
     title: string
 }
+
+export const createUserSchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    role: z.string()
+})
