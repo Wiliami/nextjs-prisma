4fdf736a317f3c945455935487b63a7b124c64e1
wiliami <wiliaup97@gmail.com>
2026-06-15 22:51:36 -0400

    update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,36 @@
 'use client'
 
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react'
 
 interface Repo {
     name: string
     description: string
 }
 
-export default function App() {  
-    const [repos, setRepos] = useState<Repo[]>([]); 
-    const [filteredRepos, setFilteredRepos] = useState<Repo[]>([]);
+export default function App() {
+    const [repos, setRepos] = useState<Repo[]>([])
+    // const [filteredRepos, setFilteredRepos] = useState<Repo[]>([])
     const [search, setSearch] = useState('')
-    
+
+    console.log('Renderizou')
+
     useEffect(() => {
         fetch('https://api.github.com/users/Wiliami/repos')
         .then(response => response.json())
         .then(data => setRepos(data))
-    })
+    }, [])
 
-    useEffect(() => {
-        setFilteredRepos(repos.filter(repo => repo.name.includes(search)))
-    }, [search])
+
+    const filteredRepos = search.length > 0
+    ? repos.filter(repo => repo.name.includes(search))
+    : [];
+
+
+    // useEffect(() => {
+    //     if(search.length) {
+    //         setFilteredRepos(repos.filter(repo => repo.name.includes(search)))
+    //     }
+    // }, [search])
 
     return (
         <div>
@@ -28,38 +38,30 @@ export default function App() {
                 type="text"
                 name='search'
                 placeholder='Buscar...'
-                onChange={e => setSearch(e.target.value)}
+                onChange={(e) => setSearch(e.target.value)}
                 value={search}
             />
 
-            {search.length > 0 ? (
-                <ul>
-                    {filteredRepos.map(repo => {
-                        return (
-                            <li key={repo.name}>
-                                {repo.name}
-                            </li>
-                        )
-                    })}
-                </ul>  
-            ) : (
-                <ul>
-                    {repos.map(repo => {
-                        return (
-                            <li key={repo.name}>{repo.name}</li>
-                        )
-                    })}
-                </ul> 
-            )}
+            {
+                search.length > 0
+                ? (
+                    <ul>
+                        {filteredRepos.map(repo => {
+                            return (
+                                <li key={repo.name}>{repo.name}</li>
+                            )
+                        })}
+                    </ul>
+                ) : (
+                    <ul>
+                        {repos.map(repo => {
+                            return (
+                                <li key={repo.name}>{repo.name}</li>
+                            )
+                        })}
+                    </ul>
+                )
+            }
         </div>
     )
 }
-
-
-/* 
-1 - Buscar dados de uma API externa
-2 - Exibir os dados na tela
-3 - Crir botão de pesquisa
-4 - Pegar o valor do input
-5 - Filtrar dados
-*/
