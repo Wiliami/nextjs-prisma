9633a9a388798437329b58edf817e5065c1b74de
wiliami <wiliaup97@gmail.com>
2026-08-19 17:30:56 -0400

    chore: update

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,7 +1,9 @@
-
+import { username } from '../store/session'
 
 export default function Users() {
     return (
-        <h1>Home</h1>        
+        <div>
+            { username.role === "admin" ? <h1>Users</h1> : <h1>Acesso negado!</h1>}
+        </div>
     )
 }
