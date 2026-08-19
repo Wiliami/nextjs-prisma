ff27a23f443b9a6a8edb81ad636f97eb41b9ed2e
wiliami <wiliaup97@gmail.com>
2026-08-19 01:49:28 -0400

    chore: add data

---
+++ src/app/data/users.ts
@@ -0,0 +1,12 @@
+export const users = [
+    { id: 1, name: "Alice Johnson", email: "alice.johnson@example.com", role: "employee" },
+    { id: 2, name: "Brian Smith", email: "brian.smith@example.com", role: "employee" },
+    { id: 3, name: "Carla Mendes", email: "carla.mendes@example.com", role: "employee" },
+    { id: 4, name: "David Lee", email: "david.lee@example.com", role: "employee" },
+    { id: 5, name: "Emma Wilson", email: "emma.wilson@example.com", role: "employee" },
+    { id: 6, name: "Farhan Khan", email: "farhan.khan@example.com", role: "employee" },
+    { id: 7, name: "Grace Park", email: "grace.park@example.com", role: "employee" },
+    { id: 8, name: "Hassan Ali", email: "hassan.ali@example.com", role: "employee" },
+    { id: 9, name: "Isabel Cruz", email: "isabel.cruz@example.com", role: "employee" },
+    { id: 10, name: "Jack Turner", email: "jack.turner@example.com", role: "employee" }
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,22 @@
-export default function App() {   
-    const products = [
-        { id: 1, name: 'Mouse', category: 'Periféricos', price: 80 },
-        { id: 2, name: 'Teclado', category: 'Periféricos', price: 100 },
-        { id: 3, name: 'Headset', category: 'Periféricos', price: 150 },
-        { id: 4, name: 'Monitor', category: 'Monitores', price: 600 },
-        { id: 5, name: 'Monitor Samsung', category: 'Monitores', price: 20 },
-        { id: 5, name: 'Mesa deslizadora', category: 'Acessórios', price: 20 },
-    ]
+import ListUsers from './list/page'
 
-    const groupedProducts = Object.groupBy(
-        products,
-        (product) => product.category
-    )
-
-    // if(groupedProducts[index].category === 'Periféricos') {
-    //     console.log(groupedProducts[index].category)
-    // }
-
-    console.log('Periféricos: ', groupedProducts.Periféricos[0].name)
-    console.log('Monitores: ', groupedProducts.Monitores)
-    console.log('Acessórios: ', groupedProducts.Acessórios)
+const username = {
+    id: 1,
+    name: "John Doe",
+    email: "john.doe@gmail.com",
+    role: "student"
+}
 
 
-    // agrupar 
-    
+function ButtonMessageError() {
+    return <button className='color: blue'>Acesso negado!</button>
+}
+
+
+export default function App() {
     return (
-     <h1>{groupedProducts.Periféricos[0].id}</h1>
+        <div>
+            {username.role === 'admin' ? <ListUsers /> : <ButtonMessageError />}
+        </div>
     )
 }
